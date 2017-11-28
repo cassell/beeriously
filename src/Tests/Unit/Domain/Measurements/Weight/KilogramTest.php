40f4e6f359c01025b274b51b6c70cf04fc2c5cb3
Andrew Cassell <alc277@gmail.com>
2017-11-27 21:50:30 -0500

    Ran CS Fixer. fixes #2

--- src/Tests/Unit/Domain/Measurements/Weight/KilogramTest.php
+++ src/Tests/Unit/Domain/Measurements/Weight/KilogramTest.php
@@ -1,18 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Beeriously\Tests\Unit\Domain\Measurements\Weight;
 
 use Beeriously\Domain\Measurements\Weight\Kilograms;
-use Beeriously\Domain\Measurements\Weight\Pounds;
 use PHPUnit\Framework\TestCase;
 
 class KilogramTest extends TestCase
 {
     public function testGetter()
     {
-        $this->assertEquals(1.0, (new Kilograms(1.0))->getValue());
+        $this->assertSame(1.0, (new Kilograms(1.0))->getValue());
     }
-
-
 }
--- src/Tests/Unit/Domain/Measurements/Weight/PoundsTest.php
+++ src/Tests/Unit/Domain/Measurements/Weight/PoundsTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Beeriously\Tests\Unit\Domain\Measurements\Weight;
@@ -9,30 +10,29 @@ use PHPUnit\Framework\TestCase;
 
 class PoundsTest extends TestCase
 {
-
     public function testGetValue()
     {
-        $this->assertEquals(0.0,(new Pounds(0))->getValue());
-        $this->assertEquals(1.0,(new Pounds(1))->getValue());
+        $this->assertSame(0.0, (new Pounds(0))->getValue());
+        $this->assertSame(1.0, (new Pounds(1))->getValue());
     }
 
     public function testNegativeThrowsException()
     {
         $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Weight may not be negative");
+        $this->expectExceptionMessage('Weight may not be negative');
         new Pounds(-0.1);
     }
 
     public function testReduceBy()
     {
         // 10 - 6.1 = 3.9
-        $this->assertEquals(3.9,(new Pounds(10))->reduceBy(new Pounds(6.1))->getValue());
+        $this->assertSame(3.9, (new Pounds(10))->reduceBy(new Pounds(6.1))->getValue());
     }
 
     public function testCantReduceBy()
     {
         $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Weight may not be negative");
+        $this->expectExceptionMessage('Weight may not be negative');
 
         // 5 - 6.1 = -1.1
         (new Pounds(5))->reduceBy(new Pounds(6.1));
@@ -40,9 +40,6 @@ class PoundsTest extends TestCase
 
     public function testFromKilos()
     {
-        $this->assertEquals(0.9979,round(Pounds::fromKilograms(new Kilograms(2.2))->getValue(),4));
+        $this->assertSame(0.9979, round(Pounds::fromKilograms(new Kilograms(2.2))->getValue(), 4));
     }
-
 }
-
-
