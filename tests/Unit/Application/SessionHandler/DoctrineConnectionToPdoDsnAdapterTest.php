536c69bd2fb5e3c9da1d5c728938aca608126d2f
Andrew Cassell <alc277@gmail.com>
2018-04-03 18:26:20 -0400

    Moved tests out of src folder

---
+++ tests/Unit/Application/SessionHandler/DoctrineConnectionToPdoDsnAdapterTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Beeriously\Tests\Unit\Application\SessionHandler;
+
+use Beeriously\Application\SessionHandler\DoctrineConnectionToPdoDsnAdapter;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\Mysqli\Driver;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class DoctrineConnectionToPdoDsnAdapterTest extends TestCase
+{
+    public function testGetUsername()
+    {
+        $adapter = new DoctrineConnectionToPdoDsnAdapter($this->getMysqlConnectionMock());
+        $this->assertSame('username', $adapter->getUsername());
+    }
+
+    public function testGetPassword()
+    {
+        $adapter = new DoctrineConnectionToPdoDsnAdapter($this->getMysqlConnectionMock());
+        $this->assertSame('password', $adapter->getPassword());
+    }
+
+    public function testGetDsn()
+    {
+        $adapter = new DoctrineConnectionToPdoDsnAdapter($this->getMysqlConnectionMock());
+        $this->assertSame('mysql:host=host_name;port=port_number;dbname=database_name', $adapter->getPdoDsn());
+    }
+
+    public function testPostgresGetDsn()
+    {
+        $adapter = new DoctrineConnectionToPdoDsnAdapter($this->getPostgresConnectionMock());
+        $this->assertSame('pgsql:host=host_name;port=port_number;dbname=database_name', $adapter->getPdoDsn());
+    }
+
+    /**
+     * @return Connection|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMysqlConnectionMock()
+    {
+        $mysqlDriverMock = $this->getMockBuilder(\Doctrine\DBAL\Driver\Mysqli\Driver::class)->disableOriginalConstructor()->getMock();
+
+        $mock = $this->getMockBuilder(Connection::class)->disableOriginalConstructor()->getMock();
+        $mock->method('getDriver')->willReturn($mysqlDriverMock);
+        $mock->method('getUsername')->willReturn('username');
+        $mock->method('getPassword')->willReturn('password');
+        $mock->method('getHost')->willReturn('host_name');
+        $mock->method('getPort')->willReturn('port_number');
+        $mock->method('getDatabase')->willReturn('database_name');
+
+        return $mock;
+    }
+
+    /**
+     * @return Connection|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getPostgresConnectionMock()
+    {
+        $mysqlDriverMock = $this->getMockBuilder(\Doctrine\DBAL\Driver\PDOPgSql\Driver::class)->disableOriginalConstructor()->getMock();
+
+        $mock = $this->getMockBuilder(Connection::class)->disableOriginalConstructor()->getMock();
+        $mock->method('getDriver')->willReturn($mysqlDriverMock);
+        $mock->method('getUsername')->willReturn('username');
+        $mock->method('getPassword')->willReturn('password');
+        $mock->method('getHost')->willReturn('host_name');
+        $mock->method('getPort')->willReturn('port_number');
+        $mock->method('getDatabase')->willReturn('database_name');
+
+        return $mock;
+    }
+}
