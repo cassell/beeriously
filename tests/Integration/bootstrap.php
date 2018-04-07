9ad814757062c72f97773f17db0e0718e6fc5f71
Andrew Cassell <alc277@gmail.com>
2018-04-07 11:59:05 -0400

    don't load .env if environment is setup

--- tests/Integration/bootstrap.php
+++ tests/Integration/bootstrap.php
@@ -6,4 +6,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__.'/../../vendor/autoload.php';
 
-(new Dotenv())->load(__DIR__.'/../../.env');
+if (!isset($_SERVER['APP_ENV'])) {
+    (new Dotenv())->load(__DIR__.'/../../.env');
+}
