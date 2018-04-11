1c7e4373cc981dd5831fbee1ba00b9001723dfbf
Andrew Cassell <alc277@gmail.com>
2018-04-10 21:47:12 -0400

    cs

--- tests/Acceptance/FeatureContext.php
+++ tests/Acceptance/FeatureContext.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace Beeriously\Tests\Acceptance;
 
 use Behat\Behat\Context\Context;
-use Behat\MinkExtension\Context\MinkContext;
-use Doctrine\ORM\EntityManager;
 
 class FeatureContext implements Context
 {
-
     /**
      * @Then /^I wait (\d+) seconds$/
      */
