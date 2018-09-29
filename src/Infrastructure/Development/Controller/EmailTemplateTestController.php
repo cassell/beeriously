d5da86528ce6fdc77222a8001f4573f55eaf0cc5
Andrew Cassell <cassell@users.noreply.github.com>
2018-09-28 22:26:23 -0400

    wip: registration email

---
+++ src/Infrastructure/Development/Controller/EmailTemplateTestController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Beeriously\Infrastructure\Development\Controller;
+
+use Beeriously\Infrastructure\Controller\AbstractController;
+use Beeriously\Tests\Helpers\TestBreweryBuilder;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmailTemplateTestController extends AbstractController
+{
+    /**
+     * @Route("/dev/email/registration", methods={"GET"})
+     */
+    public function registrationConfirmation()
+    {
+        $brewery = TestBreweryBuilder::createBrewery();
+        $owner = TestBreweryBuilder::getOwner($brewery);
+
+        //  /us/dev/email/registration
+        return $this->render('@FOSUser/Registration/email.txt.twig', [
+            'user' => $owner,
+            'confirmationUrl' => 'https://.../confirmation',
+        ]);
+    }
+}
